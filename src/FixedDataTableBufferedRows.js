76ee979ca6e7b92fe3957e5060f533329102d31a
Conor Branagan <conor.branagan@gmail.com>
2017-02-14 14:05:30 -0500

    Add rowKeyGetter to Table component. (#117)
    If you set a rowKeyGetter then the `key` value for each FixedDataTableRow can
    be overridden based on the rowIndex.
    
    This is particularly useful when using CSS animations where you only want
    transitions to animate if the data source remains the same.
    
    See also: https://github.com/facebook/fixed-data-table/issues/353

--- src/FixedDataTableBufferedRows.js
+++ src/FixedDataTableBufferedRows.js
@@ -38,6 +38,7 @@ var FixedDataTableBufferedRows = React.createClass({
     rowClassNameGetter: PropTypes.func,
     rowsCount: PropTypes.number.isRequired,
     rowHeightGetter: PropTypes.func,
+    rowKeyGetter: PropTypes.func,
     rowPositionGetter: PropTypes.func.isRequired,
     scrollLeft: PropTypes.number.isRequired,
     scrollableColumns: PropTypes.array.isRequired,
@@ -136,13 +137,14 @@ var FixedDataTableBufferedRows = React.createClass({
       var rowIndex = rowsToRender[i];
       var currentRowHeight = this._getRowHeight(rowIndex);
       var rowOffsetTop = baseOffsetTop + rowPositions[rowIndex];
+      var rowKey = props.rowKeyGetter ? props.rowKeyGetter(rowIndex) : i;
 
       var hasBottomBorder =
         rowIndex === props.rowsCount - 1 && props.showLastRowBorder;
 
       this._staticRowArray[i] =
         <FixedDataTableRow
-          key={i}
+          key={rowKey}
           isScrolling={props.isScrolling}
           index={rowIndex}
           width={props.width}
