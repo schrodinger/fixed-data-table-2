c39af346909c8d5eb914e92dee75ec4002a8fe9c
Chris Jordan <flipperkid@gmail.com>
2017-08-31 09:54:13 -0400

    tests

--- test/selectors/bufferRowsCount-test.js
+++ test/selectors/bufferRowsCount-test.js
@@ -6,26 +6,26 @@ import bufferRowsCount from 'bufferRowsCount';
 
 describe('bufferRowsCount', function() {
   it('should return half the max visible rows count', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { availableHeight: 450 }), 5);
+    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { maxAvailableHeight: 450 }), 5);
   });
 
   it('should round max visible rows count up', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { availableHeight: 425 }), 5);
+    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { maxAvailableHeight: 425 }), 5);
   });
 
   it('should round buffer rows count down', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { availableHeight: 400 }), 4);
+    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { maxAvailableHeight: 400 }), 4);
   });
 
   it('should be at least 3', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { availableHeight: 150 }), 3);
+    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { maxAvailableHeight: 150 }), 3);
   });
 
   it('should be at most 6', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { availableHeight: 650 }), 6);
+    assert.strictEqual(bufferRowsCount.resultFunc(null, 50, 0, { maxAvailableHeight: 650 }), 6);
   });
 
   it('should be settable via prop', function() {
-    assert.strictEqual(bufferRowsCount.resultFunc(1, 50, 0, { availableHeight: 450 }), 1);
+    assert.strictEqual(bufferRowsCount.resultFunc(1, 50, 0, { maxAvailableHeight: 450 }), 1);
   });
 });
