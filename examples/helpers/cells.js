029297a8910d3bee51ed012d2d726d6ff8ed1328
Brandon Yue <bryue@microsoft.com>
2018-08-21 15:16:18 -0500

    Replace legacy refs usages

--- examples/helpers/cells.js
+++ examples/helpers/cells.js
@@ -139,7 +139,7 @@ class TooltipCell extends React.PureComponent {
         {...props}
         onMouseEnter={() => { ReactTooltip.show(); }}
         onMouseLeave={() => { ReactTooltip.hide(); }}>
-        <div ref='valueDiv' data-tip={value}>
+        <div ref={(r) => this._valueDivRef = r} data-tip={value}>
           {value}
         </div>
       </Cell>
