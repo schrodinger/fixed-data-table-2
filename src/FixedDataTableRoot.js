1dc983cd52b0f439cd56f424e62c426e8d41abf4
pradeepnschrodinger <niranjan.pradeep@schrodinger.com>
2019-09-18 04:33:33 +0000

    Version 1.0.0-beta.28

--- src/FixedDataTableRoot.js
+++ src/FixedDataTableRoot.js
@@ -16,7 +16,7 @@ import Cell from 'FixedDataTableCellDefault';
 import Column from 'FixedDataTableColumn';
 import ColumnGroup from 'FixedDataTableColumnGroup';
 
-const version = '1.0.0-beta.27';
+const version = '1.0.0-beta.28';
 
 export {
   Cell,
