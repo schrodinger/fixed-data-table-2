f16ef3994fc6e00126ec416490ced8ec088151a8
Louie Bertoncin <louiebertoncin@gmail.com>
2019-10-24 13:54:47 -0500

    Removed rtl dir on the document that was inserted for testing

--- site/client.js
+++ site/client.js
@@ -4,8 +4,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var IndexPage = require('./IndexPage');
 
-document.documentElement.dir = "rtl";
-
 ReactDOM.render(
   <IndexPage
     {...window.INITIAL_PROPS}
