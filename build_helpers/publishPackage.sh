65bb90d2fc04c5ffc9007d43774b1465217a4428
pradeepnschrodinger <niranjan.pradeep@schrodinger.com>
2021-01-25 07:35:29 +0000

    remove retries for versioning and specify tags while publishing

--- build_helpers/publishPackage.sh
+++ build_helpers/publishPackage.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # example usages:
-# ./publishPackage.sh --branch=v1.0.x
-# ./publishPackage.sh --branch=v1.1.x --latest
-# ./publishPackage.sh --branch=v1.0-beta --beta
+# ./publishPackage.sh
+# ./publishPackage.sh --latest
+# ./publishPackage.sh --beta
 
 set -e
 
 # Defaults that can be overridden in the CLI (except the cookie file)
 export BETA=false
 export LATEST=false
-export BRANCH=""
 
 # Parse flags
 while (( "$#" )); do
@@ -26,15 +25,6 @@ while (( "$#" )); do
       LATEST=true
       ;;
 
-    --branch)
-      if [ -z $VALUE ] ; then
-        echo "Please specify branch name"
-        exit 192
-      fi
-      BRANCH=$VALUE;
-      shift
-      ;;
-
     * ) # Invalid flag
       echo "Invalid flag: $opt"
       exit 192
@@ -42,25 +32,15 @@ while (( "$#" )); do
   esac
 done
 
-# branch name is required
-if [ -z "$BRANCH" ] ; then
-  echo "Please specify branch name using the --branch flag"
-  exit 192
-fi
-
-current_version=$(node -p "require('./package').version")
-
 # prompt next version
-while [ true ]; do
-  printf "Next version (current is $current_version)? "
-  read next_version
-  if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
-    echo "Version must be a valid semver string, e.g. 1.0.2 or 2.3.0-beta.1"
-    continue
-  else
-    break
-  fi
-done
+current_version=$(node -p "require('./package').version")
+printf "Next version (current is $current_version)? "
+read next_version
+
+if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
+  echo "Version must be a valid semver string, e.g. 1.0.2 or 2.3.0-beta.1"
+  exit 1
+fi
 
 # npm test -- --single-run
 
@@ -100,16 +80,21 @@ git tag "$next_ref"
 git push origin "$next_ref"
 
 echo "Pushing release cut"
-git push origin $BRANCH
+git push
 
 if [ $BETA = true ]; then
   echo "Publishing beta tag"
   npm publish --tag beta
-elif [ $LATEST = true ]; then
+else
+  echo "Publishing $next_version to npm"
+  npm publish --tag $next_version
+fi
+
+if [ $LATEST = true ]; then
   echo "Tagging" $next_ref "as 'latest'"
   git tag latest -f
   git push origin latest -f
 
-  echo "Publishing latest tag"
+  echo "Publishing latest tag to npm"
   npm publish --tag latest
 fi
