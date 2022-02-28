79f805a2eb423ebea49ef346627b57ae91a80a27
Niranjan Pradeep <41563608+pradeepnschrodinger@users.noreply.github.com>
2022-02-28 16:15:17 +0530

    Update dependencies to fix security vulnerabilities (#626)
    Bumps up or replaces some of our packages to fix security vulnerabilities.
    
    The main changes are:
    * update `webpack-dev-server` and `webpack-cli` to remove dependency on `ansi-html` and indirectly update `node-forge`
    * replace `uglifyjs-webpack-plugin` with `terser-webpack-plugin` to indirectly update `serialize-javascript`
    * upgrade postcss related dependencies, and fix some related code
    * upgrade `jsdom` to indirectly update `jsprism`
    * upgrade `husky` to indirectly update `semver-regex`
    * fix `startSiteDevServer` script to work in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ var path = require('path');
 var glob = require('glob');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var packageJSON = require('./package.json');
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-var isDev = JSON.stringify(process.env.NODE_ENV !== 'production');
+var isDev = process.env.NODE_ENV !== 'production';
 
-var banner = (
+var banner =
   '/**\n' +
-  ' * FixedDataTable v' + packageJSON.version + ' \n' +
+  ' * FixedDataTable v' +
+  packageJSON.version +
+  ' \n' +
   ' *\n' +
   ' * Copyright Schrodinger, LLC\n' +
   ' * All rights reserved.\n' +
@@ -17,30 +19,23 @@ var banner = (
   ' * This source code is licensed under the BSD-style license found in the\n' +
   ' * LICENSE file in the root directory of this source tree. An additional grant\n' +
   ' * of patent rights can be found in the PATENTS file in the same directory.\n' +
-  ' */\n'
-);
+  ' */\n';
 
 var plugins = [
   new MiniCssExtractPlugin({
     filename: '[name].css',
   }),
   new webpack.DefinePlugin({
-    '__DEV__': isDev
-  })
+    __DEV__: isDev,
+  }),
 ];
 
 var entry = {};
-var baseEntryPoints = glob.sync(
-  path.join(__dirname, './src/css/layout/*.css')
-);
+var baseEntryPoints = glob.sync(path.join(__dirname, './src/css/layout/*.css'));
 
-var styleEntryPoints = glob.sync(
-  path.join(__dirname, './src/css/style/*.css')
-);
+var styleEntryPoints = glob.sync(path.join(__dirname, './src/css/style/*.css'));
 
-var mainEntryPoints = glob.sync(
-  path.join(__dirname, './src/**/*.css')
-);
+var mainEntryPoints = glob.sync(path.join(__dirname, './src/**/*.css'));
 mainEntryPoints.push('./src/FixedDataTableRoot.js');
 
 if (process.env.COMPRESS) {
@@ -53,9 +48,7 @@ if (process.env.COMPRESS) {
   entry['fixed-data-table'] = mainEntryPoints;
 }
 
-plugins.push(
-  new webpack.BannerPlugin({ banner, raw: true })
-);
+plugins.push(new webpack.BannerPlugin({ banner, raw: true }));
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -64,7 +57,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
@@ -74,12 +67,12 @@ module.exports = {
             options: {
               // only enable hot in development
               hmr: isDev,
-              fallback: 'style-loader'
-            }
+              fallback: 'style-loader',
+            },
           },
           'css-loader',
           path.join(__dirname, './build_helpers/cssTransformLoader'),
-        ]
+        ],
       },
     ],
   },
@@ -109,23 +102,15 @@ module.exports = {
   },
 
   node: {
-    Buffer: false
+    Buffer: false,
   },
 
-  plugins: plugins
+  plugins: plugins,
 };
 
 if (process.env.COMPRESS) {
   module.exports.optimization = {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compressor: {
-            warnings: false
-          },
-          output: { comments: false }
-        }
-      })
-    ]
-  }
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  };
 }
