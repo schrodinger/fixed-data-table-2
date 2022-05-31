16ad25d11191d632d0f0d85ebac734b0625ffe95
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2022-05-31 12:46:14 +0530

    Version 2.0.0-beta.8

--- src/FixedDataTableRoot.js
+++ src/FixedDataTableRoot.js
@@ -20,7 +20,7 @@ import { PluginContext } from './Context';
 import ResizeCell from './plugins/ResizeReorder/ResizeCell';
 import ReorderCell from './plugins/ResizeReorder/ReorderCell';
 
-const version = '2.0.0-beta.7';
+const version = '2.0.0-beta.8';
 
 const Plugins = {
   PluginContext,
