169e536b4e559e79f0fda7b286e8de995de55a50
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2022-06-23 20:45:32 +0530

    Version 1.2.10

--- src/FixedDataTableRoot.js
+++ src/FixedDataTableRoot.js
@@ -17,6 +17,6 @@ import DataCell from './FixedDataTableCellDefault';
 import Column from './FixedDataTableColumn';
 import ColumnGroup from './FixedDataTableColumnGroup';
 
-const version = '1.2.9';
+const version = '1.2.10';
 
 export { Cell, Column, ColumnGroup, DataCell, Table, version };
