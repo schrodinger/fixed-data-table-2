05ff5421e01112b06d7171c1d76e7ea220978f5b
Aman Gupta <aman.gupta@schrodinger.com>
2023-06-29 23:00:46 +0530

    Bump jsdom to 16.5.0 and upgrade test and related build_helpers script (#686)

--- build_helpers/test-globals.js
+++ build_helpers/test-globals.js
@@ -1,24 +1,27 @@
-'use strict'
+'use strict';
 
-var jsdom = require('jsdom');
+const { JSDOM } = require('jsdom');
 
 // setup the simplest document possible
-var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const dom = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'https://localhost/',
+  runScripts: 'outside-only',
+});
 
-// get the window object out of the document
-var win = doc.defaultView;
+// get the window and document object out of the  dom
+const { document, window } = dom.window;
 
 // set globals for mocha that make access to document and window feel
 // natural in the test environment
-global.document = doc;
-global.window = win;
+global.document = document;
+global.window = window;
 
 // take all properties of the window object and also attach it to the
 // mocha global object
-propagateToGlobal(win);
+propagateToGlobal(window);
 
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-function propagateToGlobal (window) {
+function propagateToGlobal(window) {
   for (let key in window) {
     if (!window.hasOwnProperty(key) || key in global) {
       continue;
