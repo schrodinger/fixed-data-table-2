283cc2d47e5499d01e04d6e2979d4b4ae9a5e1b2
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2023-12-03 03:25:46 +0530

    Replace npm bin with npx

--- build_helpers/buildStaticSite.sh
+++ build_helpers/buildStaticSite.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-PATH=$(npm bin):$PATH
 
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-NODE_ENV=production webpack --config "$PWD/site/webpack-client.config.js"
-NODE_ENV=production webpack --config "$PWD/site/webpack-prerender.config.js"
+NODE_ENV=production npx webpack --config "$PWD/site/webpack-client.config.js"
+NODE_ENV=production npx webpack --config "$PWD/site/webpack-prerender.config.js"
 NODE_ENV=production ./build_helpers/buildSiteIndexPages.sh
--- build_helpers/startSiteDevServer.sh
+++ build_helpers/startSiteDevServer.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-PATH=$(npm bin):$PATH
 
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-webpack --config "$PWD/site/webpack-client.config.js"
-webpack --config "$PWD/site/webpack-prerender.config.js"
+npx webpack --config "$PWD/site/webpack-client.config.js"
+npx webpack --config "$PWD/site/webpack-prerender.config.js"
 ./build_helpers/buildSiteIndexPages.sh
-webpack-dev-server --config "$PWD/site/webpack-client.config.js"
+npx webpack-dev-server --config "$PWD/site/webpack-client.config.js"
