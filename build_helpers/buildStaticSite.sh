aea3e64e7a3b8010a0cb76da106d41ad95b5cba4
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2023-12-03 03:37:02 +0530

    Remove npx since npm run adds 'node_modules/.bin' to path

--- build_helpers/buildStaticSite.sh
+++ build_helpers/buildStaticSite.sh
@@ -4,6 +4,6 @@ set -e
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-NODE_ENV=production npx webpack --config "$PWD/site/webpack-client.config.js"
-NODE_ENV=production npx webpack --config "$PWD/site/webpack-prerender.config.js"
+NODE_ENV=production webpack --config "$PWD/site/webpack-client.config.js"
+NODE_ENV=production webpack --config "$PWD/site/webpack-prerender.config.js"
 NODE_ENV=production ./build_helpers/buildSiteIndexPages.sh
--- build_helpers/startSiteDevServer.sh
+++ build_helpers/startSiteDevServer.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf ./__site__
 rm -rf ./__site_prerender__
 ./build_helpers/buildAPIDocs.sh
-npx webpack --config "$PWD/site/webpack-client.config.js"
-npx webpack --config "$PWD/site/webpack-prerender.config.js"
+webpack --config "$PWD/site/webpack-client.config.js"
+webpack --config "$PWD/site/webpack-prerender.config.js"
 ./build_helpers/buildSiteIndexPages.sh
-npx webpack-dev-server --config "$PWD/site/webpack-client.config.js"
+webpack-dev-server --config "$PWD/site/webpack-client.config.js"
