24fb75a76131ed70318abaa54fb220bbb4412caa
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2024-03-30 03:56:33 +0530

    Version 2.0.8

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import { FixedDataTableContext as Context } from './FixedDataTableContext';
 import ResizeCell from './plugins/ResizeReorder/ResizeCell';
 import ReorderCell from './plugins/ResizeReorder/ReorderCell';
 
-const version = '2.0.7';
+const version = '2.0.8';
 
 const Plugins = {
   ResizeCell,
